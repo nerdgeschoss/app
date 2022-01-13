332e30d9db631484af48410d4300db9b89abe285
Jens Ravens <jens@nerdgeschoss.de>
2022-01-13 13:39:20 +0100

    add smpt settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,13 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  ActionMailer::Base.smtp_settings = {
+    user_name: ENV["SMTP_USERNAME"],
+    password: ENV["SMTP_PASSWORD"],
+    address: ENV["SMTP_ADDRESS"],
+    port: 587,
+    authentication: :login,
+    enable_starttls_auto: true
+  }
 end
