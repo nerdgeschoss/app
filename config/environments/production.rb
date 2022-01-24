b865005e461dad401e21dd0f9021c20e51efd6bb
Jens Ravens <jens@nerdgeschoss.de>
2022-01-24 10:07:05 +0100

    update smtp settings syntax

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  ActionMailer::Base.smtp_settings = {
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
     user_name: ENV["SMTP_USERNAME"],
     password: ENV["SMTP_PASSWORD"],
     address: ENV["SMTP_ADDRESS"],
