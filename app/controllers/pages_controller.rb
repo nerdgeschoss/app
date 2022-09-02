a0a2eea58b78694d5ab791dbd77922aa0f5df4e4
Jens Ravens <jens@nerdgeschoss.de>
2022-09-02 15:25:26 +0200

    cleaned out sprint view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,7 +6,7 @@ class PagesController < ApplicationController
   def home
     @payslips = current_user.payslips.reverse_chronologic.page(0).per(6)
     @sprint = Sprint.current.take
-    @upcoming_leaves = current_user.leaves.future.limit(3)
+    @upcoming_leaves = current_user.leaves.future.chronologic
   end
 
   def offline
