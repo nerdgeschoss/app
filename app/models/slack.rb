9b874ad0d15f355cdc2755b9e0af88b0c6695ae4
Malte S <malte@nerdgeschoss.de>
2023-01-04 16:03:53 +0100

    slack api

---
+++ app/models/slack.rb
@@ -0,0 +1,26 @@
+class Slack
+  attr_accessor :body
+
+  def initialize(body)
+    @body = body
+  end
+
+	def notify
+		HTTParty.post(url, body: body.to_json, headers: headers)
+	end
+
+	private
+
+	def url
+		"https://slack.com/api/chat.postMessage"
+	end
+
+	def token
+		Config.slack_token!
+	end
+
+	def headers
+		{"Content-Type": "application/json", "authorization": "Bearer #{token}"}
+	end
+end
+
