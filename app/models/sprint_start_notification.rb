7ba4410ed1c183a46d7074d7182a252606aeffb4
Malte S <malte@nerdgeschoss.de>
2023-01-30 16:19:40 +0100

    reduce number of queries and methods

--- app/models/sprint_start_notification.rb
+++ app/models/sprint_start_notification.rb
@@ -6,13 +6,15 @@ class SprintStartNotification
   end
 
   def message
+    memoized_leaves_text_lines = leaves_text_lines
+    memoized_users = currently_employed_users
     I18n.t("sprints.notifications.sprint_start_content",
       title: @sprint.title,
       sprint_during: ApplicationController.helpers.date_range(@sprint.sprint_during.min, @sprint.sprint_during.max, format: :long),
       working_days: @sprint.working_days,
-      leaves: leaves_text_lines,
-      count: leaves_text_lines.size)
-      .concat("\n", birthdays_text_lines, anniversaries_text_lines)
+      leaves: memoized_leaves_text_lines,
+      count: memoized_leaves_text_lines.size)
+      .concat("\n", birthdays_text_lines(memoized_users), anniversaries_text_lines(memoized_users))
   end
 
   private
@@ -23,14 +25,14 @@ class SprintStartNotification
     end.join
   end
 
-  def birthdays_text_lines
-    currently_employed_users.select { |user| @sprint.sprint_during.cover?(birthday_in_actual_year(user)) || @sprint.sprint_during.cover?(birthday_in_next_year(user)) if user.born_on.present? }.map do |user|
+  def birthdays_text_lines(users)
+    users.select { |user| @sprint.sprint_during.cover?(date_in_actual_year(user.born_on)) || @sprint.sprint_during.cover?(date_in_next_year(user.born_on)) if user.born_on.present? }.map do |user|
       I18n.t("sprints.notifications.birthday_line", user: user.display_name, date: user.born_on&.strftime("%d.%m."))
     end.join
   end
 
-  def anniversaries_text_lines
-    currently_employed_users.select { |user| @sprint.sprint_during.cover?(hiring_date_in_actual_year(user)) || @sprint.sprint_during.cover?(hiring_date_in_next_year(user)) if user.hired_on.present? }.map do |user|
+  def anniversaries_text_lines(users)
+    users.select { |user| @sprint.sprint_during.cover?(date_in_actual_year(user.hired_on)) || @sprint.sprint_during.cover?(date_in_next_year(user.hired_on)) if user.hired_on.present? }.map do |user|
       I18n.t("sprints.notifications.anniversary_line", user: user.display_name, date: user.hired_on.strftime("%d.%m."))
     end.join
   end
@@ -39,19 +41,11 @@ class SprintStartNotification
     User.sprinter
   end
 
-  def birthday_in_actual_year(user)
-    user.born_on.change year: Date.current.year
+  def date_in_actual_year(date)
+    date.change year: Date.current.year
   end
 
-  def hiring_date_in_actual_year(user)
-    user.hired_on.change year: Date.current.year
-  end
-
-  def birthday_in_next_year(user)
-    user.born_on.change year: Date.current.year + 1
-  end
-
-  def hiring_date_in_next_year(user)
-    user.hired_on.change year: Date.current.year + 1
+  def date_in_next_year(date)
+    date.change year: Date.current.year + 1
   end
 end
