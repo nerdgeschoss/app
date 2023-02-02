c2003d5bf10d9f0f920544ed27a89b2d0829d441
Malte S <malte@nerdgeschoss.de>
2023-02-02 18:43:40 +0100

    correct typo

--- app/models/sprint/notification.rb
+++ app/models/sprint/notification.rb
@@ -1,15 +1,17 @@
 # frozen_string_literal: true
 
 class Sprint::Notification
+  attribute_reader :sprint
+
   def initialize(sprint)
     @sprint = sprint
   end
 
   def message
     I18n.t("sprints.notifications.sprint_start_content",
-      title: @sprint.title,
-      sprint_during: ApplicationController.helpers.date_range(@sprint.sprint_during.min, @sprint.sprint_during.max, format: :long),
-      working_days: @sprint.working_days,
+      title: sprint.title,
+      sprint_during: ApplicationController.helpers.date_range(sprint.sprint_during.min, sprint.sprint_during.max, format: :long),
+      working_days: sprint.working_days,
       leaves: leaves_text_lines,
       count: leaves_text_lines.size)
       .concat("\n", birthdays_text_lines(currently_employed_users), anniversaries_text_lines(currently_employed_users))
@@ -26,6 +28,7 @@ class Sprint::Notification
   def birthdays_text_lines(users)
     users.select { |user| range_covers_repeating_date?(range: @sprint.sprint_during, date: user.born_on) if user.born_on.present? }.map do |user|
       I18n.t("sprints.notifications.birthday_line", user: user.display_name, date: I18n.l(user.hired_on, format: :short))
+    end.join
   end
 
   def anniversaries_text_lines(users)
@@ -39,6 +42,6 @@ class Sprint::Notification
   end
 
   def range_covers_repeating_date?(range:, date:)
-    range.cover?(date.change year: Date.current.year) || range.cover?(date.change(year: Date.current.year + 1))
+    range.cover?(date.change(year: Date.current.year)) || range.cover?(date.change(year: Date.current.year + 1))
   end
 end
