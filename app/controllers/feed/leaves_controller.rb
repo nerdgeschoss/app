67bade18854f21ee928afec46cb977a6dc0b1b55
Jens Ravens <jens@nerdgeschoss.de>
2023-04-05 11:42:52 +0200

    add specs for ics feed

--- app/controllers/feed/leaves_controller.rb
+++ app/controllers/feed/leaves_controller.rb
@@ -2,9 +2,18 @@
 
 module Feed
   class LeavesController < FeedController
+    before_action :require_login
+
     def index
       @leaves = Leave.all
       respond_to :ics
+    end
+
+    private
+
+    def require_login
+      current_user = User.find_by id: params[:auth] if params[:auth].present?
+      head :unauthorized unless current_user&.roles&.include?("hr")
     end
   end
 end
