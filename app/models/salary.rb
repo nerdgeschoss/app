04110d09d031a31b00d83bf9e9f4b1d55f094d7e
Jens Ravens <jens@nerdgeschoss.de>
2023-07-07 14:51:01 +0200

    remove salary date range

--- app/models/salary.rb
+++ app/models/salary.rb
@@ -4,27 +4,23 @@
 #
 # Table name: salaries
 #
-#  id           :uuid             not null, primary key
-#  user_id      :uuid             not null
-#  valid_during :daterange        not null
-#  brut         :decimal(, )      not null
-#  net          :decimal(, )      not null
-#  hgf_hash     :string
-#  created_at   :datetime         not null
-#  updated_at   :datetime         not null
+#  id         :uuid             not null, primary key
+#  user_id    :uuid             not null
+#  brut       :decimal(, )      not null
+#  net        :decimal(, )      not null
+#  hgf_hash   :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  valid_from :date             not null
 #
 class Salary < ApplicationRecord
-  include RangeAccessing
-
   belongs_to :user
 
-  scope :chronologic, -> { order("LOWER(salaries.valid_during) ASC") }
+  scope :chronologic, -> { order(valid_from: :asc) }
 
-  range_accessor_methods :valid
-
-  validates :valid_during, :brut, :net, presence: true
+  validates :valid_from, :brut, :net, presence: true
 
   def current?
-    valid_until.nil?
+    user.salaries.max_by(&:valid_from) == self
   end
 end
