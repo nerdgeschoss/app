6dffc5f36a7ad4b2d7377f04cd77151b6e77ec45
Malte S <malte@nerdgeschoss.de>
2023-10-17 16:36:17 +0200

    add tests

--- spec/models/leave_spec.rb
+++ spec/models/leave_spec.rb
@@ -20,7 +20,7 @@ require "rails_helper"
 RSpec.describe Leave do
   fixtures :all
   let(:user) { users(:john) }
-  let(:holiday) { user.leaves.create! type: :paid, title: "Holidays", days: ["2023-01-02"] }
+  let(:holiday) { user.leaves.create! type: :paid, title: "Holidays", days: ["2023-01-02", "2023-01-03"] }
   let(:single_day_sick_leave) { user.leaves.create! type: :sick, title: "Sick", days: ["2023-01-02"] }
 
   it "single day sick leaves are automatically approved" do
@@ -32,7 +32,7 @@ RSpec.describe Leave do
     it "requests a leave from HR" do
       holiday.notify_hr_on_slack_about_new_request
       text = <<~TEXT
-        *<@slack-john>* requested a new paid leave for January 2, 2023
+        *<@slack-john>* requested a new paid leave for January 2 — 3, 2023
         You can approve or reject this request <http://localhost:31337/en/leaves?status=pending_approval&user_id=d6f57eaa-da8a-5c59-a1a1-8ebe34034b8a|here>
       TEXT
       expect(Slack.instance.last_message.text).to eq text.strip
@@ -43,7 +43,7 @@ RSpec.describe Leave do
       holiday.notify_user_on_slack_about_status_change
       text = <<~TEXT
         Hi, *John*!
-        Your paid leave request for January 2, 2023 has been approved.
+        Your paid leave request for January 2 — 3, 2023 has been approved.
       TEXT
       expect(Slack.instance.last_message.text).to eq text.strip
     end
@@ -53,7 +53,7 @@ RSpec.describe Leave do
       holiday.notify_user_on_slack_about_status_change
       text = <<~TEXT
         Hi, *John*!
-        Your paid leave request for January 2, 2023 has been rejected.
+        Your paid leave request for January 2 — 3, 2023 has been rejected.
       TEXT
       expect(Slack.instance.last_message.text).to eq text.strip
     end
@@ -68,6 +68,28 @@ RSpec.describe Leave do
         *<@slack-john> is on sick leave today*
       TEXT
       expect(Slack.instance.last_message.text).to eq text.strip
+    end
+  end
+
+  context "Slack status" do
+    it "is being set on the first day of a leave" do
+      stub_request(:post, "https://slack.com/api/users.profile.set")
+
+      travel_to "2023-01-02"
+      holiday.update! status: :approved
+
+      allow(Slack.instance).to receive(:set_status)
+      SlackSetStatusJob.perform_now
+      expect(Slack.instance).to have_received(:set_status).once
+    end
+
+    it "is not being set on any other day of a leave" do
+      travel_to "2023-01-03"
+      holiday.update! status: :approved
+
+      allow(Slack.instance).to receive(:set_status)
+      SlackSetStatusJob.perform_now
+      expect(Slack.instance).not_to have_received(:set_status)
     end
   end
 end
