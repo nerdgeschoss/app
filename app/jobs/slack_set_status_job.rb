e9e631c63f59c07838317146f4d1a101dfbc04d6
Malte S <malte@nerdgeschoss.de>
2023-10-17 16:37:42 +0200

    add status update job

---
+++ app/jobs/slack_set_status_job.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class SlackSetStatusJob < ApplicationJob
+  queue_as :notification
+  sidekiq_options retry: 0
+
+  def perform
+    Leave.approved.starts_today.each do |leave|
+      User::SlackProfile.new(leave.user).set_status(type: leave.sick? ? "sick" : "vacation", until_date: leave.leave_during.max)
+    end
+  end
+end
