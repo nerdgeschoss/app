86a348fc549d72853800489194a4e1ecb677b76f
Malte S <malte@nerdgeschoss.de>
2023-11-07 23:54:17 +0100

    add test for module

--- spec/models/leave_spec.rb
+++ spec/models/leave_spec.rb
@@ -111,4 +111,62 @@ RSpec.describe Leave do
       end
     end
   end
+
+  context "RequestHandling" do
+    let(:sick_leave) { single_day_sick_leave }
+    let(:paid_leave) { holiday }
+    let(:unpaid_leave) { holiday.tap { |l| l.type = "unpaid" } }
+
+    before do
+      allow(sick_leave).to receive(:notify_slack_about_sick_leave)
+      allow(paid_leave).to receive(:notify_hr_on_slack_about_new_request)
+      allow(paid_leave).to receive(:set_slack_status!)
+    end
+
+    describe "#handle_incoming_request" do
+      it "notifies slack about sick leave for sick type" do
+        sick_leave.handle_incoming_request
+        expect(sick_leave).to have_received(:notify_slack_about_sick_leave)
+      end
+
+      it "notifies HR on slack about new request for paid type" do
+        paid_leave.handle_incoming_request
+        expect(paid_leave).to have_received(:notify_hr_on_slack_about_new_request)
+      end
+
+      it "notifies HR on slack about new request for unpaid type" do
+        unpaid_leave.handle_incoming_request
+        expect(unpaid_leave).to have_received(:notify_hr_on_slack_about_new_request)
+      end
+
+      it "sets slack status if leave is during today" do
+        allow(Time.zone).to receive(:today).and_return(paid_leave.leave_during.min)
+        paid_leave.handle_incoming_request
+        expect(paid_leave).to have_received(:set_slack_status!)
+      end
+    end
+
+    describe "#handle_slack_status" do
+      it "sets slack status if leave is during today and approved" do
+        paid_leave.approved!
+        allow(Time.zone).to receive(:today).and_return(paid_leave.leave_during.min)
+        paid_leave.handle_slack_status
+        expect(paid_leave).to have_received(:set_slack_status!)
+      end
+
+      it "does not set slack status if leave is not during today" do
+        paid_leave.approved!
+        allow(Time.zone).to receive(:today).and_return(paid_leave.leave_during.max + 1.day)
+        paid_leave.handle_slack_status
+        expect(paid_leave).not_to have_received(:set_slack_status!)
+      end
+
+      it "does not set slack status if leave is not approved" do
+        paid_leave.pending_approval!
+        allow(Time.zone).to receive(:today).and_return(paid_leave.leave_during.min)
+        paid_leave.handle_slack_status
+        expect(paid_leave).not_to have_received(:set_slack_status!)
+      end
+    end
+  end
 end
