d6ef6d45e3c6fef6a5b1594017f78ee4012a61f2
Malte S <malte@nerdgeschoss.de>
2023-11-07 17:06:39 +0100

    test calculating work days

--- spec/models/sprint_feedback_spec.rb
+++ spec/models/sprint_feedback_spec.rb
@@ -27,16 +27,29 @@ RSpec.describe SprintFeedback do
   fixtures :all
   let(:feedback) { sprint_feedbacks(:sprint_feedback_1) }
 
-  it "calculates the costs based on the current salary" do
-    feedback.recalculate_costs
-    salary = feedback.user.salary_at(feedback.sprint.sprint_from)
-    expect(salary.brut).to eq 3800
-    expect(feedback.costs).to eq 2352.38 # 10 working days, assuming 21 working days per month
+  context "when calculating costs" do
+    it "calculates the costs based on the current salary" do
+      feedback.recalculate_costs
+      salary = feedback.user.salary_at(feedback.sprint.sprint_from)
+      expect(salary.brut).to eq 3800
+      expect(feedback.costs).to eq 2352.38 # 10 working days, assuming 21 working days per month
+    end
+
+    it "displays revenue based on turnover and costs" do
+      expect(feedback.turnover).to eq 150
+      expect(feedback.revenue.to_f).to eq(-2016.67)
+      expect(feedback.turnover_per_storypoint).to eq 18.75
+    end
   end
 
-  it "displays revenue based on turnover and costs" do
-    expect(feedback.turnover).to eq 150
-    expect(feedback.revenue.to_f).to eq(-2016.67)
-    expect(feedback.turnover_per_storypoint).to eq 18.75
+  context "when calculating days" do
+    before do
+      feedback.user.leaves.create! type: :non_working, title: "Loveparade", days: [feedback.sprint.sprint_during.min]
+      feedback.user.leaves.create! type: :paid, title: "Holidays", days: [feedback.sprint.sprint_during.max - 5.days]
+      feedback.user.leaves.create! type: :sick, title: "Sick", days: [feedback.sprint.sprint_during.max - 7.days]
+    end
+    it "returns the correct working day count" do
+      expect(feedback.working_day_count).to eq feedback.sprint.working_days - 3
+    end
   end
 end
