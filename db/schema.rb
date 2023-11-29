1b07df7c3d8394f7c63874c5deb409226511cf05
Malte Stiebritz <68898490+oma-s@users.noreply.github.com>
2023-11-29 09:58:25 +0100

    Add and display nick names for users (#72)
    * initial commit for Add and display nick names for users, fixes #71
    
    * add tests
    
    * add nick name field to user
    
    * update display name method
    
    * annotate
    
    * update form
    
    * lint
    
    * permit param
    
    * re-create schema
    
    * ensure pg stat extension (for heroku)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_10_154425) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_28_105309) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
   enable_extension "pgcrypto"
@@ -188,10 +188,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_10_154425) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
+    t.string "slack_id"
     t.date "born_on"
     t.date "hired_on"
-    t.string "slack_id"
     t.string "github_handle"
+    t.string "nick_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
