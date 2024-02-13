73fc60b9935aac8d805cbacd18ea48f8ddbeff1b
Justin Marsh <jmarsh24@gmail.com>
2024-02-13 14:41:51 +0100

    test

--- app/jobs/github_import_job.rb
+++ app/jobs/github_import_job.rb
@@ -2,6 +2,7 @@
 
 class GithubImportJob < ApplicationJob
   queue_as :import
+  limits_concurrency to: 1, key: ->(job) { job.class.name }, duration: 1.hour
 
   def perform
     Task.sync_with_github
