de7bb6ba20929b94127a018368c94a70036bbc86
Malte S <malte@nerdgeschoss.de>
2024-04-08 23:40:22 +0200

    create feedback on demand

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,8 +7,10 @@ class PagesController < ApplicationController
     @payslips = current_user.payslips.reverse_chronologic.page(0).per(6)
     @sprint = Sprint.current.take
     @upcoming_leaves = current_user.leaves.future.not_rejected.chronologic
-    sprint_feedback = current_user.sprint_feedbacks.find_by(sprint: @sprint)
-    @daily_nerd_message = authorize DailyNerdMessage.find_by(created_at: Time.zone.today.all_day, sprint_feedback:) || sprint_feedback.daily_nerd_messages.build if sprint_feedback
+    if @sprint
+      sprint_feedback = current_user.sprint_feedbacks.find_or_create_by(sprint: @sprint)
+      @daily_nerd_message = authorize DailyNerdMessage.find_by(created_at: Time.zone.today.all_day, sprint_feedback:) || sprint_feedback.daily_nerd_messages.build
+    end
   end
 
   def offline
