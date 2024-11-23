652dfd22942524b6fe0d2a0ee59a06b5b620e69a
Jens Ravens <jens@nerdgeschoss.de>
2024-11-23 13:56:03 +0100

    use action cable for realtime

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_11_23_124624) do
+ActiveRecord::Schema[8.0].define(version: 2024_11_23_125437) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "pg_catalog.plpgsql"
@@ -117,6 +117,16 @@ ActiveRecord::Schema[8.0].define(version: 2024_11_23_124624) do
     t.datetime "updated_at", null: false
     t.date "valid_from", null: false
     t.index ["user_id"], name: "index_salaries_on_user_id"
+  end
+
+  create_table "solid_cable_messages", force: :cascade do |t|
+    t.binary "channel", null: false
+    t.binary "payload", null: false
+    t.datetime "created_at", null: false
+    t.bigint "channel_hash", null: false
+    t.index ["channel"], name: "index_solid_cable_messages_on_channel"
+    t.index ["channel_hash"], name: "index_solid_cable_messages_on_channel_hash"
+    t.index ["created_at"], name: "index_solid_cable_messages_on_created_at"
   end
 
   create_table "solid_cache_entries", force: :cascade do |t|
