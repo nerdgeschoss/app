b33bee0482a1deabb20e110bbbaa43a0e7c5b883
Jens Ravens <jens@nerdgeschoss.de>
2025-10-16 14:29:53 +0200

    rubocop

--- app/models/project/harvest.rb
+++ app/models/project/harvest.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Project
   module Harvest
     extend ActiveSupport::Concern
--- db/migrate/20251016111635_project_status_and_harvest_references.rb
+++ db/migrate/20251016111635_project_status_and_harvest_references.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class ProjectStatusAndHarvestReferences < ActiveRecord::Migration[8.0]
   def change
     add_column :projects, :repository, :string
