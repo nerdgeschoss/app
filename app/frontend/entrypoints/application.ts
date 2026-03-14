ab73f47fe28eaf1effbe7df5f9b3678d84193fce
Jens Ravens <jens@nerdgeschoss.de>
2026-03-14 13:17:30 +0100

    migrate to turbo

--- app/frontend/entrypoints/application.ts
+++ app/frontend/entrypoints/application.ts
@@ -1,5 +1,4 @@
-import { Layout } from '../layout';
-import { Reaction } from '../sprinkles/reaction';
+import '@hotwired/turbo';
 import '../components/reset.scss';
 import '../../components/stack/stack.scss';
 import '../../components/logo/logo.scss';
@@ -8,6 +7,3 @@ import '../../components/text/text.scss';
 import '../../components/form_error/form_error.scss';
 import '../../components/text_field/text_field.scss';
 import '../../components/button/button.scss';
-
-const reaction = new Reaction({ layout: Layout });
-reaction.start();
