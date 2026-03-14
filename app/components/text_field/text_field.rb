d09a83740230b6facc9fe8becaa68429555f2ba2
Jens Ravens <jens@nerdgeschoss.de>
2026-03-14 12:21:53 +0100

    form component

---
+++ app/components/text_field/text_field.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+
+class Components::TextField < Components::Base
+  prop :name, _Nilable(String), default: nil
+  prop :value, _Nilable(String), default: nil
+  prop :label, _Nilable(String), default: nil
+  prop :placeholder, _Nilable(String), default: nil
+  prop :required, _Boolean, default: false
+  prop :disabled, _Boolean, default: false
+  prop :readonly, _Boolean, default: false
+  prop :errors, _Nilable(_Array(String)), default: nil
+  prop :auto_complete, _Nilable(String), default: nil
+
+  def view_template
+    div(class: "text-field__container") do
+      div(class: text_field_classes) do
+        div(class: "text-field__content") do
+          render_label if @label
+          text(block: true, color: @disabled ? "label-heading-secondary" : nil) do
+            input(
+              name: @name,
+              class: "text-field__input",
+              readonly: @readonly,
+              value: @value || "",
+              type: "text",
+              placeholder: @placeholder,
+              required: @required,
+              disabled: @disabled,
+              autocomplete: @auto_complete
+            )
+          end
+        end
+        render FormError.new(errors: @errors) if @errors&.any?
+      end
+    end
+  end
+
+  private
+
+  def text_field_classes
+    classes = ["text-field"]
+    classes << "text-field--filled" if @value.present?
+    classes << "text-field--readonly" if @readonly
+    classes << "text-field--disabled" if @disabled
+    classes << "text-field--placeholder" if @placeholder
+    classes.join(" ")
+  end
+
+  def render_label
+    label(class: label_classes) do
+      text(type: :"label-heading-primary", color: "label-heading-primary") { @label }
+    end
+  end
+
+  def label_classes
+    classes = ["text-field__label"]
+    classes << "text-field__label--disabled" if @disabled
+    classes.join(" ")
+  end
+end
--- app/frontend/entrypoints/application.ts
+++ app/frontend/entrypoints/application.ts
@@ -5,6 +5,8 @@ import '../../components/stack/stack.scss';
 import '../../components/logo/logo.scss';
 import '../../components/card/card.scss';
 import '../../components/text/text.scss';
+import '../../components/form_error/form_error.scss';
+import '../../components/text_field/text_field.scss';
 
 const reaction = new Reaction({ layout: Layout });
 reaction.start();
