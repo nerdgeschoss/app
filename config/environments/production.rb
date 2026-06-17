e676e492b5a86e67cbf190604e45ed394767bdc5
Daniel Diekmeier <daniel@nerdgeschoss.de>
2026-06-17 16:32:48 +0200

    Move to Hetzner/Dokku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,6 +17,10 @@ Rails.application.configure do
   # Turn on fragment caching in view templates.
   config.action_controller.perform_caching = true
 
+  # Ensures that a master key has been made available in ENV["RAILS_MASTER_KEY"], config/master.key, or an environment
+  # key such as config/credentials/production.key. This key is used to decrypt credentials (and other encrypted files).
+  config.require_master_key = ENV["SECRET_KEY_BASE_DUMMY"].nil?
+
   # Cache assets for far-future expiry since they are all digest stamped.
   config.public_file_server.headers = {"cache-control" => "public, max-age=#{1.year.to_i}"}
 
