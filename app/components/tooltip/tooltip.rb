174bf6b132bffeb6d21ad9c9362fbeaaeb666933
Geraldine Atayan <geraldine@nerdgeschoss.de>
2026-08-13 17:01:44 +0200

    more fixes

--- app/components/tooltip/tooltip.rb
+++ app/components/tooltip/tooltip.rb
@@ -4,10 +4,10 @@ class Components::Tooltip < Components::Base
   prop :label, _Nilable(String)
 
   def view_template(&block)
-    div(class: "tooltip") do
+    div(class: "tooltip", data: {controller: "tooltip"}) do
       div(class: "tooltip__main", &block)
       if @label
-        div(class: "tooltip__anchor") do
+        div(class: "tooltip__anchor", data: {tooltip_target: "anchor"}) do
           icon(name: "tooltip-arrow", size: 10)
           div(class: "tooltip__content") do
             text(type: "tooltip-primary", color: "tooltip-label-default", no_wrap: true) { @label }
